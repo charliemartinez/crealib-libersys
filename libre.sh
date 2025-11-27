325112b638cf03efdddbe76fdf12fdc005696206
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2025-11-27 18:58:29 +0100

    Revise script header comments in libre.sh
    Updated script header with author, license, and utility information.

--- libre.sh
+++ libre.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-# Nombre:    CREALIB LIBERSYS - Comando "libre"
-# Autor:    Charlie Martínez® <cmartinez@quirinux.org>
-# Licencia:    https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-# Descripción:    Desinstalar componentes privativos de un sistema GNU/Linux
-# Versión:    1.0
+# ==============================================================
+# Nombre:            Crealib Libersys - Comandos libre
+# Autor:             Charlie Martinez® <cmartinez@quirinux.org>
+# Licencia:          https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Utilidad:          Desinstalar componentes privativos de un sistema GNU/Linux
+# Distro:            Quirinux 2.x (Devuan 5, Devuan 6, Debian 12, Debian 13)
+# ==============================================================
+# Ejecutar con permisos de administrador
+# ==============================================================
 
 FOLDER_NONFREE="/opt/libersys"
 FILE_LIST="$FOLDER_NONFREE/list"
